666f6a4e904702ed0db248815c9a6578ef5b0a8f
Bruno Martins <bgcngm@gmail.com>
2018-09-16 01:38:48 +0100

    sdm845-common: Add XiaomiDoze app
     * Based off android_device_xiaomi_msm8996-common
    
    Change-Id: I01096d5d02669fe6b4230fd17b82fc60d4ddcaf2

--- device.mk
+++ device.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Ambient Display
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
