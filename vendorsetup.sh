1b5066099031e4b43de4b200c57148fcb01d53ba
Stargazer19 <pviraj19@gmail.com>
2019-03-26 05:14:21 +0000

    Init vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,5 +21,6 @@
 # function: add_lunch_combo generic-eng
 
 for var in eng user userdebug; do
-  add_lunch_combo havoc_santoni-$var
+  add_lunch_combo fred_santoni-$var
+  export SDCLANG=false
 done
